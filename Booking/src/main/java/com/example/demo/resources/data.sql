14e7b732386f4caa8d532d05339589c6bb7ec25a
Vineela Krishna <vineela.455@gmail.com>
2021-05-17 16:56:16 +0530

    Add files via upload
    API Implementation

---
+++ Booking/src/main/java/com/example/demo/resources/data.sql
@@ -0,0 +1,5 @@
+insert into booking(booking_name) values('Chris');
+insert into booking(booking_name) values('Martin');
+insert into booking(booking_name) values('Alison');
+insert into booking(booking_name) values('Gayle');
+insert into booking(booking_name) values('Andy');
